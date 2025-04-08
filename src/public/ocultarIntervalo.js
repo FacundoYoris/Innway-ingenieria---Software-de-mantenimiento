3f5c7b06988e5800883f9db1366e63ba4e5801cc
Facundo Yoris <yorisfacundo@gmail.com>
2025-04-08 19:20:44 -0300

    Checkbox en frontend listo(falta backend y que ande bien la parte de horas, dias, años)

--- src/public/ocultarIntervalo.js
+++ src/public/ocultarIntervalo.js
@@ -3,5 +3,27 @@ $(document).ready(function() {
     var selectedValue = $(this).val();
     $('#intervaloPeriodica, #lapsoPeriodica, #intPeriodica, #lapPeriodica').toggle(selectedValue === "Programada");
   });
+
+  const $produccion = $('#produccion');
+  const $grupoElemento = $('#grupoElemento');
+  const $grupoDescripcion = $('#grupoDescripcion');
+
+  function toggleCamposProduccion() {
+    if ($produccion.is(':checked')) {
+      $grupoElemento.hide();
+      $grupoDescripcion.hide();
+    } else {
+      $grupoElemento.show();
+      $grupoDescripcion.show();
+    }
+  }
+
+  // Ejecutar cuando se marca/desmarca
+  $produccion.on('change', toggleCamposProduccion);
+
+  // Ejecutar al cargar (por si viene ya marcado)
+  toggleCamposProduccion();
+
 });
+
 
